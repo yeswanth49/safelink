bb7cfc52d1c2564b004a58081c92c20b7ec5e3e6
Yeswanth <sinchan123v@gmail.com>
2024-12-27 22:38:24 +0530

    check.py

--- check.py
+++ check.py
@@ -1,10 +1,13 @@
-import sqlite3
+import psycopg2
 
 
-conn = sqlite3.connect('profiles.db')
+conn = psycopg2.connect('postgresql://yeswanth:aE1Nl3DJj47d3LUOvL86Xnyuv3k02PL2@dpg-ctmnetbqf0us738fggr0-a/safelink')
 
 c = conn.cursor()
 
 c.execute('select * from profiles')
 
 print(c.fetchall())
+
+c.close()
+conn.close()
